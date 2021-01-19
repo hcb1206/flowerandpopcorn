a65d3f19f81d7381c530f62a8fcf9c72e94c91c9
hcb1206 <hcb1206@gmail.com>
2021-01-19 14:33:35 -0500

    adding weighted and non-weighted graphing options

--- flowerandpopcorn_script.R
+++ flowerandpopcorn_script.R
@@ -7,6 +7,13 @@ library(RColorBrewer)
 
 data <- read_xlsx("popvsflower.xlsx", sheet = 1)
 data$flower <- data$flower * 100
+data$popcorn <- data$popcorn * 100
+
+data_weighted <- read_xlsx("popvsflower_weighted.xlsx", sheet = 1)
+data_weighted$flower <- data_weighted$flower * 100
+data_weighted$popcorn <- data_weighted$popcorn * 100
+
+#before weights
 
 gg <- ggplot(data, aes(x=flower, y=popcorn)) + 
   geom_point(aes(col=strain), size=3) + 
@@ -21,4 +28,21 @@ hh <- ggplot(data, aes(x=flower, y=popcorn)) +
   geom_smooth(method="lm", col="black", size=1) +
   theme_minimal() + 
   labs(title="Potency Ratio of Flower and Popcorn", subtitle="By Harvest", 
+       y="popcorn THC %", x="flower THC %")
+
+#after weights
+
+ggw <- ggplot(data_weighted, aes(x=flower, y=popcorn)) + 
+  geom_point(aes(col=strain), size=3) + 
+  geom_smooth(method="lm", col="black", size=1) +
+  theme_minimal() + 
+  labs(title="Weighted Potency Ratio of Flower and Popcorn", subtitle="By Strain", 
+       y="popcorn THC %", x="flower THC %")
+
+
+hhw <- ggplot(data_weighted, aes(x=flower, y=popcorn)) + 
+  geom_point(aes(col=harvest), size=3) + 
+  geom_smooth(method="lm", col="black", size=1) +
+  theme_minimal() + 
+  labs(title="Weighted Potency Ratio of Flower and Popcorn", subtitle="By Harvest", 
        y="popcorn THC %", x="flower THC %")
